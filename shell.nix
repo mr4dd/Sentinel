4098a882dde44c14d267b32d03984540ecf63ab4
mr4dd <158076664+mr4dd@users.noreply.github.com>
2026-02-27 15:37:50 +0000

    Refactor: transition to multi-threaded monitoring and network sniffing
        Implement AsyncSniffer for real-time packet capture.
    
        Add transportHandler for remote data extrusion via JSON.
    
        Refactor main to run log collection and system monitoring in parallel threads.
    
        Update systemMonitor to support continuous metric polling.
    
        Add basic KeyboardInterrupt handling.

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  python-env = pkgs.python3.withPackages (ps: with ps; [
+    requests
+    scapy 
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    python-env
+    pkgs.libpcap
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.libpcap}/lib:$LD_LIBRARY_PATH"
+    
+    echo "--- Nix Python Shell ---"
+    echo "Python: $(python --version)"
+  '';
+}
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os
 import subprocess
-#import requests
+import requests
 import time
 import threading
+from scapy.all import AsyncSniffer, Packet
+import json
+from queue import Queue
 
 class logCollectionHandler():
     def __init__(self, logType: str):
@@ -51,9 +54,16 @@ class logCollectionHandler():
 
 class systemMonitor():
     def __init__(self):
-        pass
+        self.packets = []
+        self.sniffer = AsyncSniffer(prn=self._packet_callback, store=False)
+        self.sniffer.start()
 
-    def stat(self):
+    def getPackets(self)->list[str]:
+        packets = self.packets
+        self.packets = []
+        return packets
+
+    def stat(self)->list[str]:
         metrics = []
         processes = os.listdir("/proc/")
         for process in processes:
@@ -65,21 +75,68 @@ class systemMonitor():
                 continue
         return metrics
 
-def getHandler():
+    def _packet_callback(self, packet: Packet):
+        self.packets.append(packet.json())
+    
+    def destroy():
+        self.sniffer.stop()
+
+class transportHandler():
+    def __init__(self, server: str, log: str='', stats:str='', pcap: str=''):
+        self.BackLog = Queue()
+        self.server = server
+        self.logEndpoint = log
+        self.statEndpoint = stats
+        self.pcapEndpoint = pcap
+
+    def post(self, data: str, dataType: str):
+        endpointMap = {"log":self.logEndpoint, "stat":self.statEndpoint, "pcap": self.pcapEndpoint}
+        endpoint = endpointMap.get(dataType)
+        if not endpoint:
+            raise Exception("invalid data type")
+        try:
+            requests.post(f'{server}/{endpoint}', json=data)
+        except requests.exceptions.RequestException as e:
+            self.Backlog.put({dataType:logs})
+
+
+def getHandlerType():
     keys = os.environ.keys()
     if "ANDROID_DIR" in keys:
         return "android"
     else:
         return "gnu" 
 
-def main():
-    handler = logCollectionHandler(getHandler())
-    print(systemMonitor().stat())
-    exit()
+def collectLogs():
+    handler = logCollectionHandler(getHandlerType())
     while True:
         v = handler.getLogs(200)
         if v:
-            pass
+            logsJson = json.dumps(v)
 
+def monitorSystem():
+    monitor = systemMonitor()
+    while True:
+        s = monitor.stat()
+        p = monitor.getPackets()
+        if s:
+            statJson = json.dumps(s)
+        if p:
+            pcapJson = json.dumps(p)
+            
+def main():
+    logThread = threading.Thread(target=collectLogs)
+    systemMonitorThread = threading.Thread(target=monitorSystem)
+    try:
+        logThread.start()
+        systemMonitorThread.start()
+        logThread.join()
+        systemMonitorThread.join()
+
+    except KeyboardInterrupt:
+        print("keyboard interrupt recieved, shutting down")
+        exit()
+        # i need to gracefully handle shutdown so data already in thread isnt lost and any connections that are active get closed
+        
 if __name__ == "__main__":
     main()
